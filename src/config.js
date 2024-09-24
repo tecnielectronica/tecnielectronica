54d41ab2831d4b41012d5ac3c92fc4ce5121e779
tecnielectronica <tecnielectronicaalvarez@gmail.com>
2024-09-24 02:44:12 -0500

    first commit

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/tecnidb";
-export const PORT = process.env.PORT || 3000;
+  process.env.MONGODB_URI || 
+  "mongodb://mongo:eOrYsPEcCUbHbFBcdNidHfshgzUPdTBk@autorack.proxy.rlwy.net:47971";
+export const PORT = process.env.PORT || 47971;
 export const SECRET = "yoursecretkey";
--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost/tecnidb", {
+mongoose.connect("mongodb://mongo:oAukhzNqTcIVjdbcnkgePXOIwgEokRjQ@junction.proxy.rlwy.net:40781", {
 })
     .then(db => console.log('Db is connected'))
     .catch(error => console.log(error))
